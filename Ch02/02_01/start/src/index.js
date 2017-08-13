8dd2a6820c6c861ad92ab97bd958afe2e1365b07
Yuelin37 <yuelinyan@gmail.com>
2017-08-12 21:58:53 -0600

    02_05 done

--- Ch02/02_01/start/src/index.js
+++ Ch02/02_01/start/src/index.js
@@ -2,9 +2,8 @@ const { render } = ReactDOM
 
 render(<h1 id='title'
 		className='header'
-		style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}
-	>
-	Hello World
+		style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
+	Hey World!!!
 	</h1>,
 	document.getElementById('react-container')
 )
